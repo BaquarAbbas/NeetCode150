4de410641f87f559321567f8cda6bbbb9e765252
BaquarAbbas <70859858+BaquarAbbas@users.noreply.github.com>
2022-08-07 22:48:38 +0530

    Create ContainsDuplicate.cpp
    One of the famous questions in leetcode.

---
+++ ArraysAndHashing/ContainsDuplicate.cpp
@@ -0,0 +1,16 @@
+//solution1
+//Time:O(N)
+//Space:O(N) where N is the size of the vector!.
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i = 0; i < nums.size();i++){
+            if(s.find(nums[i]) != s.end()){
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
